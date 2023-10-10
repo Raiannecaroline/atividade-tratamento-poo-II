8561b4f37d91502fdc643c652b7c834e12b1d5c9
Raiane Caroline S Teixeira <raianecaroline.st@gmail.com>
2023-10-10 20:07:18 -0300

    update: Alterações nas classes

--- CorpoDocente.java
+++ CorpoDocente.java
@@ -1,13 +1,16 @@
+import java.util.Arrays;
+
 public class CorpoDocente {
 
-    private String nome;
-    private String idade;
-    private String sexo;
-    private String cpf;
-    private String rg;
-    private String endereco;
+    private String[] nome;
+    private String[] idade;
+    private String[] sexo;
+    private String[] cpf;
+    private String[] rg;
+    private String[] endereco;
 
-    public CorpoDocente(String nome, String idade, String sexo, String cpf, String rg, String endereco) {
+    public CorpoDocente(String[] nome, String[] idade, String[] sexo,
+                        String[] cpf, String[] rg, String[] endereco) {
         this.nome = nome;
         this.idade = idade;
         this.sexo = sexo;
@@ -16,51 +19,60 @@ public class CorpoDocente {
         this.endereco = endereco;
     }
 
-    public String getNome() {
+    public String[] getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
+    public void setNome(String[] nome) {
         this.nome = nome;
     }
 
-    public String getIdade() {
+    public String[] getIdade() {
         return idade;
     }
 
-    public void setIdade(String idade) {
+    public void setIdade(String[] idade) {
         this.idade = idade;
     }
 
-    public String getSexo() {
+    public String[] getSexo() {
         return sexo;
     }
 
-    public void setSexo(String sexo) {
+    public void setSexo(String[] sexo) {
         this.sexo = sexo;
     }
 
-    public String getCpf() {
+    public String[] getCpf() {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
+    public void setCpf(String[] cpf) {
         this.cpf = cpf;
     }
 
-    public String getRg() {
+    public String[] getRg() {
         return rg;
     }
 
-    public void setRg(String rg) {
+    public void setRg(String[] rg) {
         this.rg = rg;
     }
 
-    public String getEndereco() {
+    public String[] getEndereco() {
         return endereco;
     }
 
-    public void setEndereco(String endereco) {
+    public void setEndereco(String[] endereco) {
         this.endereco = endereco;
+    }
+
+    public void listarProfessores() {
+        System.out.println("Nome: " + Arrays.toString(this.nome));
+        System.out.println("Idade: " + Arrays.toString(this.idade));
+        System.out.println("Sexo: " + Arrays.toString(this.sexo));
+        System.out.println("CPF: " + Arrays.toString(this.cpf));
+        System.out.println("RG: " + Arrays.toString(this.rg));
+        System.out.println("Endereço: " + Arrays.toString(this.endereco));
     }
 }
--- Dependentes.java
+++ Dependentes.java
@@ -1,12 +1,12 @@
 public class Dependentes extends CorpoDocente{
 
 
-    private String nome;
-    private String idade;
-    private String sexo;
+    private String[] nome;
+    private String[] idade;
+    private String[] sexo;
 
 
-    public Dependentes(String nome, String idade, String sexo, String cpf, String rg, String endereco) {
+    public Dependentes(String[] nome, String[] idade, String[] sexo, String[] cpf, String[] rg, String[] endereco) {
         super(nome, idade, sexo, cpf, rg, endereco);
         this.nome = nome;
         this.idade = idade;
@@ -14,32 +14,32 @@ public class Dependentes extends CorpoDocente{
     }
 
     @Override
-    public String getNome() {
+    public String[] getNome() {
         return nome;
     }
 
     @Override
-    public void setNome(String nome) {
+    public void setNome(String[] nome) {
         this.nome = nome;
     }
 
     @Override
-    public String getIdade() {
+    public String[] getIdade() {
         return idade;
     }
 
     @Override
-    public void setIdade(String idade) {
+    public void setIdade(String[] idade) {
         this.idade = idade;
     }
 
     @Override
-    public String getSexo() {
+    public String[] getSexo() {
         return sexo;
     }
 
     @Override
-    public void setSexo(String sexo) {
+    public void setSexo(String[] sexo) {
         this.sexo = sexo;
     }
 }
